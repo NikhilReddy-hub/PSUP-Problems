883f775b0661c8c4a21f7fd137836a78e23e0e0b
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2024-10-16 15:33:17 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,74 @@
+#BMI
+# hei = int(input())
+# wei = int(input())
+# a = (hei*hei)
+# bmi = (wei/a)
+# print(bmi)
+
+#simpleinterest
+# principle=float(input(""))
+# time=int(input(""))
+# rate=float(input(""))
+# simple_interest=(principle*time*rate)/100
+# print("",simple_interest)
+
+#Area of Circle
+#from math import pi
+# rad = float(input())
+# area = pi * r ** 2
+# print(str(area))
+
+#find acceleration using distance and time
+# t=float(input())
+# dis=float(input())
+# acc=((2 * distance) / (time ** 2))
+# print(f"The acceleration is: {acc:.2f} m/s^2")
+
+#velocity using mass and force
+# mass = float(input())
+# for = float(input())
+# tim = float(input())
+# v1 = float(input())
+# acce = f/ m
+# vel = v1+ acc* t
+# print(vel)
+
+#print * in a pyramid pattern
+# row = int(input(""))
+# a = 0
+# for i in range(1, rows+1):
+#     for space in range(1, (rows-i)+1):
+#         print(end="  ")
+#     while a!=(2*i-1):
+#         print("* ", end="")
+#         a += 1
+#     a = 0
+#     print()
+
+
+#convert kms to miles
+# km = float(input(""))
+# conversion_factor = 0.621371
+# miles = km * conversion_factor
+# print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
+#programme to find joules
+# for=float(input())
+# dist=float(input())
+# energy=(for*dist)
+# print(energy)
+
+#program to find distance between two points using Euler's Distance Formula
+def euler_distance(x1, y1, x2, y2):
+return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+x1 = float(input(""))
+y1 = float(input(""))
+x2 = float(input(""))
+y2 = float(input(""))
+distance = euler_distance(x1, y1, x2, y2)
+print(f"{distance:.2f}")
+
+
+
+
+
